87f7f897c624257af3a85c61a17483de7cee16e4
vinaqn <vina3nguyen@gmail.com>
2025-12-17 19:18:45 -0800

    convert unix date to timestamp

--- transform/dbt/earthquake/models/staging/staging_earthquake.sql
+++ transform/dbt/earthquake/models/staging/staging_earthquake.sql
@@ -1,17 +1,17 @@
 {{
-    config(
-        materialized="incremental",
-        unique_key=["id"],
-        incremental_strategy="delete+insert"
-        )
+  config(
+    materialized="incremental",
+    unique_key="earthquake_id",
+    incremental_strategy="delete+insert"
+  )
 }}
 
 select
-    id as "earthquake_id",
-    cursor_time
-    --{{dbt_date.from_unixtimestamp("cursor_time",format="milliseconds")}} as "updated"
+  id as earthquake_id,
+  cast(cursor_time as integer) as cursor_time_ms,
+  {{ dbt_date.from_unixtimestamp("cursor_time_ms", format="milliseconds") }} as updated
 from {{ source('earthquake', 'earthquake') }}
 
 {% if is_incremental() %}
-    where cursor_time > (select max(cursor_time) from {{ this }} )
+where cursor_time > (select max(cursor_time) from {{ this }})
 {% endif %}
